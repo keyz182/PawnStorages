81a5e30320275b8989a4329f7b1e33806a1d3c73
Dexter Lowe <feldoh10@gmail.com>
2024-09-18 21:47:34 +0100

    Fixup lazies

--- 1.5/Source/PawnStorages/PawnStoragesModule-DefensivePositions/CompPawnStorage_Patch.cs
+++ 1.5/Source/PawnStorages/PawnStoragesModule-DefensivePositions/CompPawnStorage_Patch.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using DefensivePositions;
 using HarmonyLib;
-using RimWorld;
 using UnityEngine;
 using Verse;
 
@@ -13,41 +11,44 @@ namespace PawnStorages.DefensivePositions;
 [HarmonyPatch(typeof(CompPawnStorage))]
 public static class CompPawnStorage_Patch
 {
+    private static Lazy<MethodInfo> hasSavedPositionMethod = new(() => AccessTools.Method(typeof(PawnSavedPositionHandler), "HasSavedPosition"));
+    private static Lazy<MethodInfo> getPositionMethod = new(() => AccessTools.Method(typeof(PawnSavedPositionHandler), "GetPosition"));
+    private static Lazy<MethodInfo> draftToPositionMethod = new(() => AccessTools.Method(typeof(PawnSavedPositionHandler), "DraftToPosition"));
+    private static Lazy<Texture2D> releaseToDefensivePositionTex = new(() => ContentFinder<Texture2D>.Get("UIPositionLargeActive", true));
+
     [HarmonyPatch(nameof(CompPawnStorage.CompGetGizmosExtra))]
     [HarmonyPostfix]
     public static IEnumerable<Gizmo> CompGetGizmosExtra_Patch(IEnumerable<Gizmo> __result, CompPawnStorage __instance)
     {
         foreach (Gizmo g in __result) yield return g;
-        Lazy<Command_Action> action = new Lazy<Command_Action>(() => new Command_Action
+        if (__instance.innerContainer.Count <= 0) yield break;
+
+        Command_Action action = new()
         {
             defaultLabel = "PS_ReleaseAllToDefensivePositions".Translate(),
-            defaultDesc = "PS_ReleaseAllToDefensivePositions_Tooltop".Translate(),
-            icon = ContentFinder<Texture2D>.Get("UIPositionLargeActive", true)
-        });
-        action.Value.action = delegate
-        {
-
-            for (int num = __instance.innerContainer.Count - 1; num >= 0; num--)
+            defaultDesc = "PS_ReleaseAllToDefensivePositions_Tooltip".Translate(),
+            icon = releaseToDefensivePositionTex.Value,
+            action = delegate
             {
-                Pawn pawn = (Pawn) __instance.GetAt(num);
-                Utility.ReleasePawn(__instance, pawn, __instance.parent.Position, __instance.parent.Map);
-
-                PawnSavedPositionHandler mgr = DefensivePositionsManager.Instance.GetHandlerForPawn(pawn);
-
-                Lazy<MethodInfo> hasSavedPositionMethod = new(()=>typeof(PawnSavedPositionHandler).GetMethod("HasSavedPosition", BindingFlags.NonPublic | BindingFlags.Instance));
-                Lazy<MethodInfo> getPositionMethod = new(()=>typeof(PawnSavedPositionHandler).GetMethod("GetPosition", BindingFlags.NonPublic | BindingFlags.Instance));
-                Lazy<MethodInfo> draftToPositionMethod = new(()=>typeof(PawnSavedPositionHandler).GetMethod("DraftToPosition", BindingFlags.NonPublic | BindingFlags.Instance));
-
-                if (hasSavedPositionMethod.Value == null || getPositionMethod.Value == null || draftToPositionMethod.Value == null || !(bool) hasSavedPositionMethod.Value.Invoke(mgr, [0]))
+                for (int num = __instance.innerContainer.Count - 1; num >= 0; num--)
                 {
-                    continue;
-                }
+                    Pawn pawn = (Pawn) __instance.GetAt(num);
+                    Utility.ReleasePawn(__instance, pawn, __instance.parent.Position, __instance.parent.Map);
 
-                IntVec3 pos = (IntVec3) getPositionMethod.Value.Invoke(mgr, [0]);
-                draftToPositionMethod.Value.Invoke(mgr, [pos]);
-                DefensivePositionsManager.Instance.Reporter.ReportPawnInteraction(ScheduledReportManager.ReportType.SentToSavedPosition, pawn, true, 0);
+                    PawnSavedPositionHandler mgr = DefensivePositionsManager.Instance.GetHandlerForPawn(pawn);
+
+                    if (hasSavedPositionMethod.Value == null || getPositionMethod.Value == null || draftToPositionMethod.Value == null ||
+                        !(bool) hasSavedPositionMethod.Value.Invoke(mgr, [0]))
+                    {
+                        continue;
+                    }
+
+                    IntVec3 pos = (IntVec3) getPositionMethod.Value.Invoke(mgr, [0]);
+                    draftToPositionMethod.Value.Invoke(mgr, [pos]);
+                    DefensivePositionsManager.Instance.Reporter.ReportPawnInteraction(ScheduledReportManager.ReportType.SentToSavedPosition, pawn, true, 0);
+                }
             }
         };
-        yield return action.Value;
+        yield return action;
     }
 }
